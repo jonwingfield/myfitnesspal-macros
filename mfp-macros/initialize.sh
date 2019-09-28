6587e177533aa4923a5cf15f468fd0b9fac7dcdb
Jon Wingfield <wingfield.jon@gmail.com>
2019-09-27 23:01:36 -0400

    Need to build in a docker container because of native dependencies (lxml).

--- mfp-macros/initialize.sh
+++ mfp-macros/initialize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir packages
-pip3 install -r requirements.txt  -t packages
+# Spin up a docker container to pull in the dependencies so we can build on Amazon Linux for the native libs
+docker run --rm -v "$PWD":/var/task lambci/lambda:build-python3.7 pip3 install -r requirements.txt -t packages
