f41aa51fb904d2b37a734af7115f392aaa39a93e
Jon Wingfield <wingfield.jon@gmail.com>
2019-09-27 23:01:50 -0400

    Hard-code timezone to EST for me.

--- mfp-macros/handler.py
+++ mfp-macros/handler.py
@@ -4,18 +4,18 @@ parent_dir = os.path.abspath(os.path.dirname(__file__))
 packages_dir = os.path.join(parent_dir, 'packages')
 sys.path.append(packages_dir)
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import myfitnesspal
 
 client = myfitnesspal.Client(os.environ['USER_NAME'], os.environ['PASSWORD'])
 
-def handle(req):
+def handle(event, lambda_context):
     try:
-        day = client.get_date(datetime.now())
-        print(day.__dict__)
+        now_est = datetime.now() - timedelta(hours=5)
+        day = client.get_date(now_est)
         totals = day.totals
 
-        return "{} grams of protein, {} calories".format(totals['calories'], totals['protein'])
+        return "{} grams of protein, {} calories".format(totals['protein'], totals['calories'])
     except KeyError:
         return "no food logged today yet"
     except Exception as e:
